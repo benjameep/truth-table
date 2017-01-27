831d8d6ac9794805f0eee55ab943e92241a8a7af
Benjamin Earl <benearl72@gmail.com>
2017-01-26 18:59:30 -0700

    Initial Commit

---
+++ truthTable.py
@@ -0,0 +1,39 @@
+import sys
+
+while True:
+
+    x = input("\n\nEnter Calculation: ").upper()
+
+    vars = []
+    for i in x:
+        print(i,end=' ')
+        if i.isalpha() and not i in vars:
+            vars.append(i)
+
+    for i in range(2**len(vars)):
+        print('')
+        string = bin(i)[2:].rjust(len(vars),'0')
+        bools = list(map((lambda x: True if x=='1' else False), string))
+        a = dict(zip(vars,bools))
+        math = []
+        for j in x:
+            if j.isalpha():
+                math.append(a[j])
+                print(int(a[j]),end=' ')
+            elif j == '&':
+                math[-2] = math[-2] & math[-1]
+                print(int(math[-2]),end=' ')
+                del math[-1]
+            elif j == '|':
+                math[-2] = math[-2] | math[-1]
+                print(int(math[-2]),end=' ')
+                del math[-1]
+            elif j == '^':
+                math[-2] = math[-2] ^ math[-1]
+                print(int(math[-2]),end=' ')
+                del math[-1]
+            elif j == '!' or j == '~' or j == '`':
+                math[-1] = not math[-1]
+                print(int(math[-1]),end=' ')
+            else:
+                print("Error: Invalid Character")
